0d5d10ff72883e74e5005d876ea7c50bc595bab6
Tharun1718333 <f20190190@hyderabad.bits-pilani.ac.in>
2022-06-15 15:46:41 +0530

    added extra test case

--- hello.go
+++ hello.go
@@ -3,6 +3,9 @@ package main
 const EnglishGreeeting = "Hello, "
 
 func Hello(k string) string {
+	if k == "" {
+		return EnglishGreeeting + "World"
+	}
 	return EnglishGreeeting + k
 }
 func main() {
--- hello_test.go
+++ hello_test.go
@@ -3,9 +3,18 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+	t.Run("saying hello world when empty", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
 }
