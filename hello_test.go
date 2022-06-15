1654a44a8673603ae92171dee8125b3130da8bf6
Tharun1718333 <f20190190@hyderabad.bits-pilani.ac.in>
2022-06-15 15:56:56 +0530

    refactored some error methods

--- hello_test.go
+++ hello_test.go
@@ -3,18 +3,20 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
+	AssesrtCorrectMessage := func(t testing.TB, got string, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want % q", got, want)
+		}
+	}
 	t.Run("saying hello to people", func(t *testing.T) {
 		got := Hello("Chris")
 		want := "Hello, Chris"
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		AssesrtCorrectMessage(t, got, want)
 	})
 	t.Run("saying hello world when empty", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, World"
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		AssesrtCorrectMessage(t, got, want)
 	})
 }
