04aec23d0a1b550eff956402fd866bb62ea1b76f
Tharun1718333 <f20190190@hyderabad.bits-pilani.ac.in>
2022-06-15 16:14:06 +0530

    added a new language  functionality

--- hello.go
+++ hello.go
@@ -1,12 +1,22 @@
 package main
 
-const EnglishGreeeting = "Hello, "
+const EnglishGreeting = "Hello, "
+const SpanishGreeting = "Hola, "
 
-func Hello(k string) string {
-	if k == "" {
-		return EnglishGreeeting + "World"
+func Hello(k string, language string) string {
+	if language == "" {
+		if k == "" {
+			return EnglishGreeting + "World"
+		}
+		return EnglishGreeting + k
 	}
-	return EnglishGreeeting + k
+	if language == "Spanish" {
+		if k == "" {
+			return SpanishGreeting + "World"
+		}
+		return SpanishGreeting + k
+	}
+	return k
 }
 func main() {
 	//fmt.Println(Hello())
--- hello_test.go
+++ hello_test.go
@@ -10,13 +10,18 @@ func TestHello(t *testing.T) {
 		}
 	}
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "")
 		want := "Hello, Chris"
 		AssesrtCorrectMessage(t, got, want)
 	})
 	t.Run("saying hello world when empty", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
+		AssesrtCorrectMessage(t, got, want)
+	})
+	t.Run("test in spanish", func(t *testing.T) {
+		got := Hello("Eldoe", "Spanish")
+		want := "Hola, Eldoe"
 		AssesrtCorrectMessage(t, got, want)
 	})
 }
