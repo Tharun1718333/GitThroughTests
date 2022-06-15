60e88afd94d0d0b44215cbe721533c8f626736e6
Tharun1718333 <f20190190@hyderabad.bits-pilani.ac.in>
2022-06-15 14:53:12 +0530

    just the initial commit with some changes

---
+++ hello.go
@@ -0,0 +1,8 @@
+package main
+
+func Hello(k string) string {
+	return "Hello, " + k
+}
+func main() {
+	//fmt.Println(Hello())
+}
---
+++ hello_test.go
@@ -0,0 +1,11 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	got := Hello("Chris")
+	want := "Hello, Chris"
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
